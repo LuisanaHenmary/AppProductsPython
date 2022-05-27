a6207b29c048970cf802f370973b81808b14281a
Luisana Perez <luisanahenmaryp@gmail.com>
2022-05-27 17:53:28 -0400

    Create FieldEdit

--- components/field.py
+++ components/field.py
@@ -1,3 +1,4 @@
+from itertools import tee
 import tkinter as tkr
 
 class Field:
@@ -43,3 +44,42 @@ class Field:
         """
         self.__value.delete(0,tkr.END)
 
+class FieldEdit:
+    #Esta clase es para crear un objecto que sirve como componente para la aplicacion
+    """
+        Es un campo que muestra el viejo valor de un producto
+        y tiene un campo de texto para ingresar un nuevo valor
+        __new_value: Representa el nuevo valor que se ingresa.
+    """
+
+    def __init__(self, containe, name, old_value, i, j):
+        """
+            Inicializa el objecto y lo ubica
+        """
+        #La etiqueta
+        tkr.Label(containe, text = name).grid(row = i, column = j)
+
+        #Un indicador de viejo valor
+        tkr.Entry(
+            containe,
+            text = f"Old {name}",
+            textvariable = tkr.StringVar(containe, value = old_value),
+            state = 'readonly' 
+        ).grid(row = i, column = j + 1)
+
+        #Campo de texto para ingresar el nuevo valor
+        self.__new_value = Field(containe = containe, name = f"New {name}", i = i + 1, j = j)
+
+    def get_value(self):
+        """
+            Devuelve el valor que se ingreso en el
+            campo de texto
+        """
+        return self.__new_value.get_value()
+
+    def get_length(self):
+        """
+            Devuelve la longitud fel valor que se 
+            ingreso en el campo de texto
+        """
+        return self.__new_value.get_length()
